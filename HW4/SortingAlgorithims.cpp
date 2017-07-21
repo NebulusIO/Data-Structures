5f89d7ece3590b2a8a99f3897b3ed09edfeed269
troykirin <troykirin@gmail.com>
2017-07-21 11:35:54 -0700

    inital add
    Source of HW4

---
+++ HW4/SortingAlgorithims.cpp
@@ -0,0 +1,257 @@
+#include "SortingAlgorithims.h"
+
+
+SortAlg::SortAlg()
+{
+  //constructor
+}
+
+SortAlg::~SortAlg()
+{
+  //destructor
+}
+
+SortAlg::SortAlg(SortAlg &copy)
+{
+  //copy constructor
+}
+
+void SortAlg::Set(string inc_val_n_loc){
+    string temp_string = inc_val_n_loc;
+
+    int new_start = 0;
+    int end_loc = 0;
+    int num_of_items = 0;
+
+    for(int j = 0; j < (signed)temp_string.size(); j++) //walk through string to find ; indicating number of items to create
+    {
+        if (temp_string[j] == ';') {num_of_items++;}
+    }
+    num_of_items++;//to account for last element not ending with ';'
+
+
+     vector<Item> working_vector(num_of_items);//vector of size 1, will add dynamically
+
+    for(int i = 0; i < num_of_items;i++)
+    {
+        end_loc = temp_string.find(";");
+        new_start = end_loc;
+        new_start++;
+        working_vector[i].segment = temp_string.substr(0, end_loc);
+
+        temp_string = temp_string.substr(new_start);// remove segment from temp_string
+
+        working_vector[i].value = stoi(working_vector[i].segment.substr(0,working_vector[i].segment.find(","))); //value from segment to working value
+        int index_start = working_vector[i].segment.find(",");
+        index_start++;
+        working_vector[i].index = stoi(working_vector[i].segment.substr(index_start)); //index from segment to working index
+    }
+
+    m_string = working_vector;
+
+
+}//end set
+
+void SortAlg::insertion(){
+    vector<Item> work_vec = this->m_string;//copy m_string to work_vec
+
+    int p = 0;//first iterator for passes
+    int j = 0;//second iterator for going back
+
+    ///testing
+    cout << "Set is now: " << endl;
+           for(int i = 0; (unsigned)i < work_vec.size(); i++)
+            cout << work_vec[i].value << "," << work_vec[i].index << endl;
+    ///
+
+
+    int array_size = work_vec.size(); //number of items in work_vec
+
+    ///test
+    cout << "array_size" << array_size << endl;
+    ///
+
+    for(p = 1; p < array_size; ++p) //pass for number of items
+    {
+        Item temp = std::move(work_vec[p]); //store value at p to temp
+
+        ///
+        //cout << temp.value << "," << temp.index << ";";
+        ///
+
+        for(j = p; j > 0 && temp.value < work_vec[j - 1].value; --j) //set j to p ; loop j until it checks all items going back & while current item is less than the prev item
+        {
+            work_vec[j] = std::move(work_vec[j-1]); //bring prev item forward 1
+            ///work_vec[j].index = temp.index;
+        }
+         ///maybe remove
+         ///temp.index = work_vec[j].index;//fix index before swap
+         work_vec[j] = std::move(temp); //bring current item back 1
+    }
+
+    ///print insertion
+    cout << "Set is now: " << endl;
+           for(int i = 0; (unsigned)i < work_vec.size(); i++)
+            cout << work_vec[i].value << "," << work_vec[i].index << ";" << endl;;
+    //
+}//end insertion
+
+void SortAlg::shell(int gap1, int gap2, int gap3){
+    vector<Item> work_vec = m_string;
+
+    cout << "running shell" << endl;
+
+    //sort by gap
+    //use modified insertion sort
+    int array_size = work_vec.size();
+    int p;
+    int j;
+
+   ///testing
+    cout << "Set is now: " << endl;
+           for(int i = 0; (unsigned)i < work_vec.size(); i++)
+            cout << work_vec[i].value << "," << work_vec[i].index << endl;
+    ///
+
+    for(int p = gap1; p < array_size; p+=gap1)
+    {
+        Item temp = std::move(work_vec[p]);
+
+        for(j = p; j > 0 && temp.value < work_vec[j-gap1].value; j-=gap1)
+        {
+            work_vec[j] = std::move(work_vec[j-gap1]);
+        }
+            work_vec[j] = std::move(temp);
+    }
+
+    ///gap2
+    for(int p = gap2; p < array_size; p+=gap2)
+    {
+        Item temp = std::move(work_vec[p]);
+
+        for(j = p; j > 0 && temp.value < work_vec[j-gap2].value; j-=gap2)
+        {
+            work_vec[j] = std::move(work_vec[j-gap2]);
+        }
+            work_vec[j] = std::move(temp);
+    }
+
+    ///gap3
+    for(int p = gap3; p < array_size; p+=gap3)
+    {
+        Item temp = std::move(work_vec[p]);
+
+        for(j = p; j > 0 && temp.value < work_vec[j-gap3].value; j-=gap3)
+        {
+            work_vec[j] = std::move(work_vec[j-gap3]);
+        }
+            work_vec[j] = std::move(temp);
+    }
+
+    ///print shell
+    cout << "Set is now: " << endl;
+           for(int i = 0; (unsigned)i < work_vec.size(); i++)
+            cout << work_vec[i].value << "," << work_vec[i].index << ";" << endl;;
+    //
+
+}//end shell
+
+void SortAlg::merge_sort() {
+
+    vector<Item> tempArr1 = this->m_string;
+    vector<Item> tempArr2(tempArr1.size());
+
+    merge_sort(tempArr1, tempArr2, 0, tempArr1.size()-1);
+
+    ///print final merge
+    cout << "Final Set is now:------------------------------------------------------------------ " << endl;
+           for(int i = 0; (unsigned)i < tempArr1.size(); i++)
+            cout << tempArr1[i].value << "," << tempArr1[i].index << ";" << endl;;
+    //
+}
+
+void SortAlg::merge_sort(vector<Item> & arr1, vector<Item> & arr2, int left, int right){
+    if(left < right){
+        int center = (left + right) / 2;
+        merge_sort(arr1, arr2, left, center);//merge sort left subtree
+        merge_sort(arr1, arr2, center+1, right);//merge_sort right subtree
+        merge_(arr1,arr2,left,center+1,right); //merges two subarrays together
+    }
+
+}
+
+void SortAlg::merge_(vector<Item> & arr1, vector<Item> & arr2, int leftPos, int rightPos, int rightEnd){
+    int leftEnd = rightPos -1;
+    int tempPos = leftPos;
+    int numElements = rightEnd - leftPos +1;
+
+    while(leftPos <= leftEnd && rightPos <= rightEnd){
+        if(arr1[leftPos].value <= arr1[rightPos].value){ ///move whichever position is less than
+            arr2[tempPos++] = move(arr1[leftPos++]);
+        }
+        else{
+        arr2[tempPos++] = move(arr1[rightPos++]);
+        }
+    }
+
+    while(leftPos <= leftEnd){ ///exhausted right subarray so add the rest of left array
+        arr2[tempPos++] = move(arr1[leftPos++]);
+    }
+
+    while(rightPos <= rightEnd){ ///exhausted left subarray so add the rest of right array
+        arr2[tempPos++] = move(arr1[rightPos++]);
+    }
+
+    for(int i = 0; i < numElements; i++ , --rightEnd){
+        arr1[rightEnd] = move(arr2[rightEnd]);
+    }
+}
+
+int SortAlg::partition_(std::vector<Item> & v, int left, int right){
+  /*
+  takes an index of an array
+  iterates through the array
+  compares if index is less than partitionvalue or greater than
+
+  repeat
+  */
+
+  int pivot = (left + right) / 2; //find the midpoint / median
+
+  while(left < right){
+    if(v[left].value < v[pivot].value)
+      left++;
+    if(v[right].value > v[pivot].value)
+      right--;
+    if(v[left].value > v[pivot].value){
+      Item temp = move(v[left]);
+      v[left] = move(v[right]);
+      v[right] = move(temp);
+    }
+    if(v[right].value < v[pivot].value){
+      Item temp = move(v[right]);
+      v[right] = move(v[left]);
+      v[left] = move(temp);
+    }
+
+  }//end while
+  return left; //returns the pivot index of recursive call
+}//end partition_
+
+void SortAlg::quick_sort(){ //driving function w/ copy string & print functionality
+  std::vector<Item> v = this->m_string; //copy over string to leave original m_string alone
+
+  quick_sort(v, 0, v.size() - 1);
+
+}//end quick_sort -- driver
+
+void SortAlg::quick_sort(std::vector<Item> & v, int left, int right){ //begin recursive function
+  if(left >= right) //base case for recursive function
+    return;
+
+  pivot = (left + right) / 2; //find the midpoint / median for pivot
+  index = partition_(v, left, right);
+  quick_sort(v, left, index - 1);
+  quick_sort(v, index, right);
+
+}//end quick_sort -- recursive part
---
+++ HW4/main.cpp
@@ -0,0 +1,99 @@
+
+// Francis Troy Kirinhakone, 11429784
+// HW #4
+// CptS 223 - Advanced Data Structures
+
+#include "SortingAlgorithims.h"
+
+#include <iostream> //std::cout std::endl;
+#include <string> //std::string
+#include <cstdlib> //std::find;
+#include <vector>
+
+using std::cout; using std::endl;
+using std::string;
+using std::find;
+
+int main (void)
+{
+
+//print name
+cout << "Francis Troy Kirinhakone, 11429784" << endl;
+
+//create array
+SortAlg workArray;;
+
+vector<Item> my_Array;
+
+//stay in command loop until quit() is entered
+    while(true)
+    {
+        string cmd; //command string
+        getline(std::cin, cmd);//get character input from terminal and assign to cmd
+
+        //finds and assigns the parameters to c_str_par
+            string parameter = ""; //new string "paramater" itilitzed to empty string
+
+
+            parameter = cmd.substr(cmd.find("("));//substring from first '(' to null terminator
+            parameter = parameter.substr(1, parameter.length()-2);//substring from +1 of prev string to -2 of length of string to remove 2nd ')'
+
+
+        if(cmd.find("quit()") == 0) {break;} //breaks cmd loop and returns Done
+
+        else if(cmd.find("set(") == 0) { //parameter (num1Val,num1Index; ...)
+            workArray.Set(parameter);
+        }
+
+        else if(cmd.find("insertion(") == 0)
+        {
+            workArray.insertion();
+        }
+
+        else if(cmd.find("shell(") == 0)
+        {
+            int gap_1, gap_2, gap_3; //initilize gap vals
+            string temp_str = ""; //temp string for stoi
+
+            //convert string of gap vals to int vals
+            temp_str = parameter.substr(0, parameter.find(","));
+            gap_1 = stoi(temp_str);
+            parameter = parameter.substr(2);
+
+            temp_str = parameter.substr(0, parameter.find(","));
+            gap_2 = stoi(temp_str);
+            parameter = parameter.substr(2);
+
+            temp_str = parameter.substr(0, parameter.find(","));
+            gap_3 = stoi(temp_str);
+
+            ///testing
+            cout << "gaps:" << gap_1 << "," << gap_2 << "," << gap_3 << endl;
+            //
+
+            //run shell()
+            workArray.shell(gap_1, gap_2, gap_3);
+
+
+        }
+
+        else if(cmd.find("merge_sort(") == 0)
+        {
+            workArray.merge_sort();
+        }
+
+        else if(cmd.find("partition(") == 0)
+        {
+
+        }
+
+        else if(cmd.find("quick_sort(") == 0)
+        {
+
+        }
+
+        else if(cmd.find("print(") == 0) {workArray.printArray();}
+
+    }
+    return 1;
+}
